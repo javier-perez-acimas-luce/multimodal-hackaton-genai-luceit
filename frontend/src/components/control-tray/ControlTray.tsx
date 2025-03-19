bb4c89b32467d49e1b0d14192a3416dc03c40662
Samuel <student-03-1b19e3b9b6bd@qwiklabs.net>
2025-03-19 16:38:41 +0000

    content type changed

--- frontend/src/components/control-tray/ControlTray.tsx
+++ frontend/src/components/control-tray/ControlTray.tsx
@@ -88,16 +88,10 @@ function ControlTray({
     const formData = new FormData();
     formData.append("file", file);
     formData.append("fileName", file.name);
-    const config = {
-      headers: {
-        "content-type": "multipart/form-data",
-      },
-    };
 
     fetch(`${serverURL}file-upload`, {
       method: "POST",
       body: formData,
-      headers: config.headers,
     }).then((response) => {
       console.log(response);
     });
